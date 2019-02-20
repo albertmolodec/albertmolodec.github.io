b7b54a38ac5ca1dacdeda48ae8ca465c6e89a3e1
Albert Abdulmanov <albert.abdu@gmail.com>
2019-02-21 02:11:57 +0500

    🖼️ Добавляет figcaption изображениям

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import './src/utils/theme.css';
-import './src/utils/fonts.css';
+import './src/utils/normalize.css';
 import './src/utils/global.css';
-
+import './src/utils/fonts.css';
+import './src/utils/theme.css';
