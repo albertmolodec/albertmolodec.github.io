5a2ea922ff167913811a48bd200b1b606756a4da
Albert Abdulmanov <albert.abdu@gmail.com>
2019-11-27 11:08:49 +0300

    Update blog-post.jsx

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -17,7 +17,7 @@ function BlogPostTemplate({ pageContext, data }) {
         title={post.frontmatter.title}
         keywords={[`блог`, `gatsby`, `javascript`, `react`]}
       />
-      <p style={{ marginBottom: "20px" }}>Я еще не работал над типографикой, поэтому посты выглядят не очень. Когда-нибудь (скоро) ситуация станет лучше.</p>
+      <p style={{ marginBottom: "20px", fontWeight: "bold" }}>Я еще не работал над типографикой, поэтому посты выглядят не очень. Когда-нибудь (скоро) ситуация станет лучше.</p>
       <article className="article">
         <div className="content">
           <h1>{post.frontmatter.title}</h1>
