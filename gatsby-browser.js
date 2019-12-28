20a67b5f4e0dd832e96046655b39646fbfab10ab
Albert Abdulmanov <albert.abdu@gmail.com>
2019-12-28 05:56:37 +0300

    Add variable font for code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
-import './src/utils/reset.css';
-import './src/utils/normalize.css';
-import './src/utils/global.css';
-import './src/utils/fonts.css';
-import './src/utils/theme.css';
+import '~src/utils/reset.css';
+import '~src/utils/code.css';
+import '~src/utils/global.css';
+import '~src/utils/fonts.css';
--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -1,38 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 // https://usehooks.com/useMedia
 
 export default function useMedia(queries, values, defaultValue) {
-  const [value, setValue] = useState(defaultValue);
-
   const mediaQueryLists = queries.map(q => window.matchMedia(q));
 
-  // State update function
-  const getValue = () => {
-    // Get index of first media query that matches
+  const getValue = useCallback(() => {
     const index = mediaQueryLists.findIndex(mql => mql.matches);
-    // Return related value or defaultValue if none
     return typeof values[index] !== 'undefined' ? values[index] : defaultValue;
-  };
+  }, [defaultValue, mediaQueryLists, values]);
 
-  useEffect(
-    () => {
-      // Set the initial value
-      setValue(getValue);
+  const [value, setValue] = useState(getValue);
 
-      // Event listener callback
-      // By defining getValue outside of useEffect we ensure that it has ...
-      // ... current values of hook args (as this hook only run on mount/dismount).
-      const handler = () => setValue(getValue);
-
-      // Set a listener for each media query with above handler as callback.
-      mediaQueryLists.forEach(mql => mql.addListener(handler));
-
-      // Remove listeners on cleanup
-      return () => mediaQueryLists.forEach(mql => mql.removeListener(handler));
-    },
-    [getValue, mediaQueryLists], // Empty array ensures effect is only run on mount and unmount
-  );
+  useEffect(() => {
+    const handler = () => setValue(getValue);
+    mediaQueryLists.forEach(mql => mql.addListener(handler));
+    return () => mediaQueryLists.forEach(mql => mql.removeListener(handler));
+  }, [getValue, mediaQueryLists]);
 
   return value;
 }
