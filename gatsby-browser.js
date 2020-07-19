6bc748146bacab9ebd8b449ebc2008095aa50cb4
Albert Abdulmanov <albert.abdulmanov@team.wrike.com>
2020-07-19 06:12:26 +0500

    Заменяет фреймворк и инструмент для CI

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-import '~src/utils/reset.css';
-import '~src/utils/code.css';
-import '~src/utils/global.css';
-import '~src/utils/fonts.css';
--- gatsby-node.js
+++
@@ -1,3 +0,0 @@
-exports.createPages = require('./config/gatsby/createPages');
-exports.onCreateNode = require('./config/gatsby/onCreateNode');
-exports.onCreateWebpackConfig = require('./config/gatsby/onCreateWebpackConfig');
