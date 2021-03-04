755558a6f45986a8cadbc94637e2e2dd77451e9a
Albert Abdulmanov <albert.abdu@gmail.com>
2021-03-05 02:33:51 +0500

    update deps, do small improvement in build script

--- esbuild.js
+++ esbuild.js
@@ -7,13 +7,18 @@ function getApiDomain() {
   return process.env.API_DOMAIN
 }
 
-;(async () => {
+function getEnvironmentVariables() {
   const define = {}
+
   for (const k in process.env) {
     define[`process.env.${k}`] = JSON.stringify(process.env[k])
   }
   define['process.env.API_DOMAIN'] = JSON.stringify(getApiDomain())
 
+  return define
+}
+
+;(async () => {
   let entryPoints = await glob('src/js/**/*.js')
   try {
     await build({
@@ -21,7 +26,7 @@ function getApiDomain() {
       outdir: 'bundle/js',
       bundle: true,
       watch: process.env.WATCH === 'true',
-      define,
+      define: getEnvironmentVariables(),
     })
   } catch (error) {
     console.error(error)
