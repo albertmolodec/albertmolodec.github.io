4f09a363eb9a531bacbde377b9e19bd9cdf2aaaf
Albert Abdulmanov <albert.abdu@gmail.com>
2021-04-11 21:01:17 +0500

    improve css, install typescript eslint

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,14 +2,15 @@ import { ComponentChildren } from 'preact'
 
 interface Props {
   children: ComponentChildren
+  className?: string
 }
 
-const Layout = ({ children }: Props) => {
-  return (
-    <div className="mx-auto max-w-screen-md px-4 sm:px-6 md:px-8 my-12 sm:my-20 md:my-32">
-      <main>{children}</main>
-    </div>
-  )
+const Layout = ({ children, className = '' }: Props) => {
+  const layoutClass =
+    'mx-auto max-w-screen-md px-4 sm:px-6 md:px-8 my-12 sm:my-20 md:my-32' +
+    className
+
+  return <main className={layoutClass}>{children}</main>
 }
 
 export default Layout
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,24 +5,21 @@ import { socials } from './socials'
 
 export default function Home() {
   return (
-    <Layout>
+    <Layout className="text-gray-900">
       <div className="flex flex-col sm:flex-row">
         <img src={avatar} className="w-48 h-48" alt="My low poly photo" />
-        <div className="mt-8 sm:mt-0 sm:ml-8 flex flex-col justify-center">
-          <p className="text-gray-900 leading-tight text-2xl md:text-3xl">
-            Hello, I'm
-          </p>
-          <h1 className="text-gray-900 leading-tight font-semibold text-4xl md:text-5xl">
+
+        <div className="leading-tight mt-8 sm:mt-0 sm:ml-8 flex flex-col justify-center">
+          <p className="text-2xl md:text-3xl">Hello, I'm</p>
+          <h1 className="font-semibold text-4xl md:text-5xl">
             Albert Abdulmanov.
           </h1>
         </div>
       </div>
-      <p className="mt-6 text-lg leading-7 text-gray-900">
+      <p className="mt-6 text-lg">
         This is my little corner on the World Wide Web.
       </p>
-      <p className="mt-6 text-lg leading-7 text-gray-900">
-        You can also find me at:
-      </p>
+      <p className="mt-2 text-lg">You can also find me at:</p>
       <div className="mt-2 flex space-x-4">
         {socials.map(Social => (
           <a
