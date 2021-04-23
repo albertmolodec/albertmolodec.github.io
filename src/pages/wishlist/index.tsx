e438f2e0f0e157eca52df7dde2f21ff9d3cda0c1
Albert Abdulmanov <albert.abdu@gmail.com>
2021-04-24 02:15:48 +0500

    move old

--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -1,3 +1,9 @@
-const Wishlist = () => <h1>Wishlist</h1>
+import Layout from '../../components/layout'
+
+const Wishlist = () => (
+  <Layout>
+    <h1>Wishlist</h1>
+  </Layout>
+)
 
 export default Wishlist
