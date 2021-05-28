23a47f5827e78c3d4e52d10a8a865ca447412b68
Albert Abdulmanov <albert.abdu@gmail.com>
2021-05-29 01:54:28 +0500

    drop all config files and sharec

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['preact', 'prettier'],
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    'prefer-template': 'off',
-  },
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'jit',
   darkMode: false,
-  purge: ['./src/**/*.{js,jsx,ts,tsx,svg,md}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx,svg}'],
   theme: {
     colors,
   },
